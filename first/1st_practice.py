956e7e301246600b35b490adfde125c34a2e7425
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-01 00:59:14 +0800

    printing, strings, and integers

--- first/1st_practice.py
+++ first/1st_practice.py
@@ -1,2 +1,13 @@
-print("Hello")
-print("123")
+name = "Matthew Desabelle"
+
+# strings
+print(name)
+print("Hello, " + name + ". How are you?")
+
+# integers
+number = 23
+print("Your age is " + str(number))
+
+# printing the data type
+print(type(name))
+print(type(number))
