d8ec4f025e2369c07a9a5836620e04961f4ad055
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-01 01:07:59 +0800

    common data types practice

--- first/1st_practice.py
+++ first/1st_practice.py
@@ -1,13 +1,22 @@
-name = "Matthew Desabelle"
-
 # strings
+name = "Matthew Desabelle"
 print(name)
 print("Hello, " + name + ". How are you?")
 
 # integers
-number = 23
-print("Your age is " + str(number))
+age = 23
+print("Your age is " + str(age))
+
+# floats
+price = 50.25
+print("The price for this snack is " + str(price) + " PHP")
+
+# boolean
+human = True
+print("Are you a human? " + str(human))
 
 # printing the data type
 print(type(name))
-print(type(number))
+print(type(age))
+print(type(price))
+print(type(human))
