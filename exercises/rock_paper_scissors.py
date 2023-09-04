d2b64d3391d9ffe7987600afd73383216e220d03
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-05 00:19:33 +0800

    changed random module

--- exercises/rock_paper_scissors.py
+++ exercises/rock_paper_scissors.py
@@ -1,9 +1,5 @@
 import random
 
-# rock beats scissors
-# scissors beats rock
-# paper beats rock
-
 
 def rock_paper_scissors_game():
     choice = ["rock", "paper", "scissors"]
@@ -15,19 +11,19 @@ def rock_paper_scissors_game():
         else:
             print("rock, paper, or scissors?")
 
-    rand = random.randint(0, 2)
+    ai_choice = random.choice(choice)
 
     print("You choose: " + user_choice)
-    print("AI choose: " + choice[rand])
+    print("AI choose: " + ai_choice)
 
-    if user_choice == choice[rand]:
+    if user_choice == ai_choice:
         print("\nYou Tied!")
     else:
-        if user_choice == "rock" and choice[rand] == "scissors":
+        if user_choice == "rock" and ai_choice == "scissors":
             print("\nYou Win!")
-        elif user_choice == "paper" and choice[rand] == "rock":
+        elif user_choice == "paper" and ai_choice == "rock":
             print("\nYou Win!")
-        elif user_choice == "scissors" and choice[rand] == "paper":
+        elif user_choice == "scissors" and ai_choice == "paper":
             print("\nYou Win!")
         else:
             print("\nYou Lose!")
