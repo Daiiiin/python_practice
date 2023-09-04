e8188290a7e24e13f5f468a60dffde7a937db2e0
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-05 01:56:49 +0800

    added file_practice

---
+++ exercises/file_practice.py
@@ -0,0 +1,25 @@
+import random
+
+# WRITING LIST
+with open("../text_files/file_practice.txt", "w") as f:
+    myList = ["Apple", "Orange", "Banana", "Grapes", "Watermelon"]
+    for x in myList:
+        f.write(x + "\n")
+    f.close()
+
+rand = random.randint(1, len(myList))
+
+with open("../text_files/file_practice.txt", "r") as f:
+    content = f.readlines()
+    print(content)
+    print(type(content))
+    f.close()
+
+with open("../text_files/file_practice.txt", "r") as f:
+    line = f.readlines()[rand - 1]
+
+    print(rand)
+    print(myList)
+    print(type(line))
+    f.close()
+
