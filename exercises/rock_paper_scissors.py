d11bae7834437a8dad5bb7c274f81ff28471bd91
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-05 00:12:59 +0800

    finished

--- exercises/rock_paper_scissors.py
+++ exercises/rock_paper_scissors.py
@@ -1,5 +1,9 @@
 import random
 
+# rock beats scissors
+# scissors beats rock
+# paper beats rock
+
 
 def rock_paper_scissors_game():
     choice = ["rock", "paper", "scissors"]
@@ -8,7 +12,25 @@ def rock_paper_scissors_game():
         user_choice = input("What is your move? ").lower()
         if user_choice in choice:
             break
+        else:
+            print("rock, paper, or scissors?")
 
+    rand = random.randint(0, 2)
+
+    print("You choose: " + user_choice)
+    print("AI choose: " + choice[rand])
+
+    if user_choice == choice[rand]:
+        print("\nYou Tied!")
+    else:
+        if user_choice == "rock" and choice[rand] == "scissors":
+            print("\nYou Win!")
+        elif user_choice == "paper" and choice[rand] == "rock":
+            print("\nYou Win!")
+        elif user_choice == "scissors" and choice[rand] == "paper":
+            print("\nYou Win!")
+        else:
+            print("\nYou Lose!")
 
 
 print("Rock, Paper, or Scissors?")
