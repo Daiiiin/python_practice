d64f3ff10db9aa931e8aebb4008b44147e2f5b99
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-04 23:38:44 +0800

    added rock_paper_scissors file

---
+++ exercises/rock_paper_scissors.py
@@ -0,0 +1,14 @@
+import random
+
+
+def rock_paper_scissors_game():
+    choice = ["rock", "paper", "scissors"]
+
+    while True:
+        user_choice = input("What is your move? ").lower()
+
+
+
+
+print("Rock, Paper, or Scissors?")
+rock_paper_scissors_game()
