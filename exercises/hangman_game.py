82aa139aa979c6182ebb406d60f2c8477b1b05a9
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-06 22:40:24 +0800

    changed randomizer in generate_word function

--- exercises/hangman_game.py
+++ exercises/hangman_game.py
@@ -12,8 +12,7 @@ import random
 def generate_word():
     with open("../text_files/hangman_words.txt", "r") as fl:
         content = fl.readlines()
-        rand = random.randint(1, len(content))
-        word = content[rand]
+        word = random.choice(content)
         fl.close()
 
     return word
